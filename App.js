e1063ecd4cddb8890f8d9f6a5bc35899ab4395b1
bharathwaj511 <151706736+bharathwaj511@users.noreply.github.com>
2023-12-29 14:58:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import Project5 from './Project5';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Project4 from './Project4';
+// import Project4Home from './Project4Home';
+// import Project4About from './Project4About';
+// import Project3 from './Project3';
+// import Project1 from './Project1';
+// import Project2 from './Project2';
+export default function App() {
+  // const myList = [
+  //   { id: 'a', value: 'apple' },
+  //   { id: 'b', value: 'banana' },
+  //   { id: 'c', value: 'orange' },
+  //   { id: 'd', value: 'grapes' },
+  //   { id: 'e', value: 'mango' },
+  // ];
+  return (
+    <div className="App">
+      {/* <Project1 myList={myList}/> */}
+      {/* <Project2/> */}
+      {/* <Project3/> */}
+      {/* <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Project4 />} />
+            <Route path='/home' element={<Project4Home/>}></Route>
+            <Route path='/about' element={<Project4About/>}></Route>
+          </Routes>
+      </BrowserRouter> */}
+      {/* <Project5/> */}
+    </div>
+  );
+}
